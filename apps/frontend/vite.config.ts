3ccbf27158bd98a79a48a75092d93e3b1c7ff268
Hariharan Selvaraj <hariharan.2309.s@icloud.com>
2025-06-16 20:06:46 +0200

    setup prettier and linter

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -1,6 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -10,6 +10,7 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
       '@components': path.resolve(__dirname, './src/components'),
       '@assets': path.resolve(__dirname, './src/assets'),
-    }
-  }
-})
+      '@api': path.resolve(__dirname, './src/api'),
+    },
+  },
+});
