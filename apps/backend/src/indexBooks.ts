69fb2b7cfc5f425c956fa9f64edb503a13c0c1f9
Hariharan Selvaraj <hariharan.2309.s@icloud.com>
2025-08-07 23:45:38 +0200

    fix

--- apps/backend/src/indexBooks.ts
+++ apps/backend/src/indexBooks.ts
@@ -1,6 +1,6 @@
 import fs from 'fs';
 import { parse } from 'csv-parse';
-import * as tf from '@tensorflow/tfjs';
+import * as tf from '@tensorflow/tfjs-nodeee';
 import * as use from '@tensorflow-models/universal-sentence-encoder';
 import { Pinecone } from '@pinecone-database/pinecone';
 import dotenv from 'dotenv';
