66b14874af3fa45af558d6117f45d090a49eebe9
Hariharan Selvaraj <hariharan.2309.s@icloud.com>
2025-09-04 12:59:19 +0200

    setting topK value to 50 - frontend

--- apps/frontend/src/components/SearchForm.tsx
+++ apps/frontend/src/components/SearchForm.tsx
@@ -43,7 +43,7 @@ const SearchForm: React.FC<SearchFormProps> = ({
     resetScroll();
 
     try {
-      const data = await fetchRecommendations(input, deviceId || '', 51);
+      const data = await fetchRecommendations(input, deviceId || '', 50);
       if (data.recommendations && Array.isArray(data.recommendations)) {
         setAllRecommendations(data.recommendations);
       } else {
@@ -72,7 +72,7 @@ const SearchForm: React.FC<SearchFormProps> = ({
 
   return (
     <motion.div
-      className={`z-51 fixed top-[190px] left-4 right-4 w-[70%] sm:w-[50%] bg-green-500/40
+      className={`z-50 fixed top-[190px] left-4 right-4 w-[70%] sm:w-[50%] bg-green-500/40
         backdrop-blur-lg border border-dashed border-green-300/20 shadow-lg
         shadow-green-500/10 flex justify-center items-center min-h-[75px]
         ${isSticky ? 'shadow-2xl shadow-green-500/20' : 'shadow-lg shadow-green-500/10'}`}
